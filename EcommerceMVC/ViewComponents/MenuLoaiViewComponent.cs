347b2f3cf7da48029ba1fa8da135c7c05df3661c
dahgit <nguyenhoangdang205@gmail.com>
2025-01-24 02:36:41 +0700

    upload

---
+++ EcommerceMVC/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceMVC.Data;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+        public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+
+         //invoke dùng để trả về view component
+        public IViewComponentResult Invoke()
+        {
+
+            var items = db.Loais.Select(loai => new MenuLoaiViewModel
+            {
+                MaLoai = loai.MaLoai,
+                TenLoai = loai.TenLoai,
+                SoLuong = loai.HangHoas.Count
+            });
+            // Default.cshtml 
+            return View("Default", items);
+            //hoặc 
+            //return View(items);
+        }
+    }
+}
