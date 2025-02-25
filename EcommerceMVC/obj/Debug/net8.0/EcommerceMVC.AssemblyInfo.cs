1268f2c38d6d63ddf5b1ad823b5be2aee9ad1054
hoangdang20 <nguyenhoangdang205@gmail.com>
2025-02-26 02:22:22 +0700

    Update .gitignore to exclude additional debug files and update project directory paths

--- EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
+++ EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347b2f3cf7da48029ba1fa8da135c7c05df3661c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
